99392ea725450340eba2fae351d51e8460947261
aparkhi83 <aparkhi8304@gmail.com>
2024-12-10 10:50:43 +0530

    commit

---
+++ lib/signin_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple,
+              Colors.purple,
+              Colors.pink,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'Sign In',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
